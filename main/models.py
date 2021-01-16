ddcfeb86d5680c369889e7030b9eef799e02f0fb
Begimai <sally.vatanabe.2002@gmail.com>
2021-01-16 18:02:32 +0600

    added todo model and in admin

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+#переведие полей задач
+
+class ToDo(models.Model): #models предоставил сам django
+    text = models.CharField(max_length=100) #текст самой задачи # максимальная длина=100 символов
+    created_at = models.DateField(auto_now_add=True) #атрибут позволит дату создания создавать самому сайту
+    is_closed = models.BooleanField(default=False)  #по умолчанию задача не помечена
+    is_favorite = models.BooleanField(default=False) 
+    
