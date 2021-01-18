b83087f34d2c0ff2308fda9a152331e92ddde1d0
Begimai <sally.vatanabe.2002@gmail.com>
2021-01-18 21:28:42 +0600

    added todo marking

--- main/views.py
+++ main/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, HttpResponse, redirect
-from .models import ToDo 
+from .models import *
 
 def homepage(request):
     return render(request, 'index.html')
 
 
-def test(request):
-    todo_list = ToDo.objects.all()
-    return render(request, 'test.html',{'todo_list':todo_list}) 
+def book_store(request):
+    book_list = BookStore.objects.all()
+    return render(request, 'book.html',{'book_list':book_list}) 
 
 def second(request):
-    return HttpResponse('test 2 page')   
+    return HttpResponse('test 2 page')  
+
+def test(request):
+    todo_list = ToDo.objects.all()
+    return render(request, 'test.html',{'todo_list':todo_list})  
+
 
 
 def add_todo(request):
@@ -27,3 +32,15 @@ def delete_todo(request, id):
     todo = ToDo.objects.get(id=id)
     todo.delete()
     return redirect(test) 
+
+def mark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = True
+    todo.save()
+    return redirect(test) 
+
+def unmark_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_favorite = False
+    todo.save()
+    return redirect(test)  
